275e2ef71898530b9f675ee479d96837323218df
yuseung Lee <yuseung0429@naver.com>
2024-03-28 00:19:46 +0900

    [Init] 프로젝트 초기화

---
+++ src/main/java/com/mirae/commerce/MiraeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mirae.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraeCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiraeCommerceApplication.class, args);
+    }
+
+}
